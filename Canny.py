96d52bd3f7796df899deb67906af75d15a94589b
root <root@DESKTOP-PD2M0EB.localdomain>
2025-08-22 16:59:47 -0600

    Agregue comentarios

--- Canny.py
+++ Canny.py
@@ -1,9 +1,13 @@
 import cv2
 
+# Leer la imagen 'cartas.jpg' en escala de grises
 img = cv2.imread('cartas.jpg', 0)
 bordeCanny = cv2.Canny(img, 100, 200)
 
+# Mostrar la imagen original
 cv2.imshow('Oringinal', img)
+
+# Mostrar la imagen con bordes detectados
 cv2.imshow('blur', bordeCanny)
 
 cv2.waitKey(0)
